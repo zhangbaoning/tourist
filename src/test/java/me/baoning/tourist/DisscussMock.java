f1b6f6d5db16dbde7b62473a1fbeb199bd516871
zhangbaoning <zhangbaoning@vip.qq.com>
2019-02-12 21:08:03 +0800

    完成爬虫

--- src/test/java/me/baoning/tourist/DisscussMock.java
+++ src/test/java/me/baoning/tourist/DisscussMock.java
@@ -6,6 +6,8 @@ import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.pipeline.ConsolePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.util.List;
+
 /**
  * @author: zhangbaoning
  * @date: 2019/2/12
@@ -19,14 +21,18 @@ public class DisscussMock implements PageProcessor {
     @Override
     // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
-        page.addTargetRequests(page.getHtml().links().regex("code=jumpnewstemplate&siteid=[0-9]+&channelid=[0-9]+&newsid=[a-z0-9]+").all());
-        //  page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
-        page.putField("name", page.getHtml().xpath("//div[@class='fulong_news_content']/p/text()").toString());
-        System.out.println(page.getHtml());
-        if (page.getResultItems().get("name") == null) {
-            //skip this page
-            page.setSkip(true);
+        List<String> urlList = page.getHtml().links().regex("code=jumpnewstemplate&siteid=[0-9]+&channelid=[0-9]+&newsid=[a-z0-9]+").all();
+        for (int i = 0; i <urlList.size() ; i++) {
+            String url = urlList.get(i);
+            url = url.replace("jumpnewstemplate","publish_40288074580926c1015818c41a9a0104");
+            String completeUrl = "http://www.xian-tourism.com/cms/show.action?"+url;
+            urlList.set(i,completeUrl);
         }
+        page.addTargetRequests(urlList);
+        //  page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
+        page.putField("name", page.getHtml().xpath("//div[@class='fulong_news_content']").smartContent());
+        //System.out.println(page.getHtml());
+
         //page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));    }
 
     }
@@ -42,7 +48,7 @@ public class DisscussMock implements PageProcessor {
                 .addUrl("http://www.xian-tourism.com/xiangcun/show.action?code=publish_4028807457e519330157ffc1239d002b&siteid=100002")
                 //开启5个线程抓取
                 .addPipeline(new ConsolePipeline())
-                .thread(5)
+                .thread(1)
                 //启动爬虫
                 .run();
     }
